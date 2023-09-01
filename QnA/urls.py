ea97a166a92eb37412a3ee929c1f907aed466cf3
Shinhyosung <tlsgytjd0218@naver.com>
2023-09-01 14:36:14 +0900

    u

--- QnA/urls.py
+++ QnA/urls.py
@@ -7,4 +7,5 @@ app_name = 'QnA'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:q_id>/', views.question, name='question'),
+    path('<int:q_id>/next_question/', views.next_question, name='next_question'),
 ]
--- QnA/views.py
+++ QnA/views.py
@@ -1,25 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Question
 from .forms import QuestionForm
 # Create your views here.
 
 def index(request):
     questions = Question.objects.all()
+    first_question = Question.objects.get(id=1)
 
     context = {
         'questions': questions,
+        'q_id': first_question.id,
     }
 
     return render(request, 'index.html', context)
 
+
+
 def question(request, q_id):
     question = Question.objects.get(id=q_id)
-    # if request.method == 'POST':
-    #     pass
-    # else:
-        
+    
     context = {
         'question': question,
+        'q_id' : question.id,
+        
     }
 
     return render(request, 'question.html', context)
+
+
+
+
+def next_question(request, q_id):
+    next_question = Question.objects.get(id=2)
+
+
+
+    return redirect(f'/QnA/{next_question.id}/')
